6b43c3221567d87a3f0a8345e97b5f2c66fb15ba
Risa Ohara <risa.ohara@gmail.com>
2015-06-22 18:48:34 -0400

    added Whitenoise package, and gitignore

--- opendata_fda/wsgi.py
+++ opendata_fda/wsgi.py
@@ -24,8 +24,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opendata_fda.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
